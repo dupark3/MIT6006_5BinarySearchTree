5de2e40810df3ee4d365814c6f9d625ccdfaddd8
DuParkLinux <dupark3@gmail.com>
2018-05-04 11:09:41 -0400

    Insert function working. Print_sorted function implemented to print the left-most leaf, then inverse-recursively print values in sorted order

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,7 +1,6 @@
 #include <iostream>
 
 /* TODO : 
-        Insert recursion
         Print recursion
         Keep a count of duplicates
 */
@@ -16,12 +15,12 @@ public:
     BinarySearchTree(T val) : root(new Node<T>(val)) { }
 
     void insert(const T&);
-    void print();
+    void print_sorted();
 
 private:
     Node<T>* root;
     void insert(Node<T>*&, Node<T>*&, const T&);
-    void print(Node<T>*);
+    void print_sorted(Node<T>*);
 };
 
 
@@ -44,15 +43,12 @@ template <class T>
 void BinarySearchTree<T>::insert(const T& val){
     if (root){ // if root exists, check val against root's value. then recursive call
         if(val < root->value){
-            std::cout << "Going Left" << std::endl;
             insert(root->left, root, val);            
         }
         else if (val > root->value){
-            std::cout << "Going Right" << std::endl;
             insert(root->right, root, val);
         }
     } else { // empty tree, insert value here
-        std::cout << "Inserting at Root" << std::endl;
         root = new Node<T>(val);
     }
 }
@@ -61,29 +57,30 @@ template <class T>
 void BinarySearchTree<T>::insert(Node<T>*& child, Node<T>*& parent, const T& val){
     if (child){
         if(val < child->value){
-            insert(child->left, parent, val);
+            insert(child->left, child, val);
         } else if (val > child->value){
-            insert(child->right, parent, val);
+            insert(child->right, child, val);
         }
     } else { // node does not exist, child is where we insert new child
         if (val < parent->value){
-            std::cout << val << " Connecting parent to left child" << std::endl;
             child = new Node<T>(val);
             parent->left = child;
         } else if (val > parent->value){
-            std::cout << "Connecting parent to right child" << std::endl;
-            parent->right = new Node<T>(val);
+            child = new Node<T>(val);
+            parent->right = child;
         }
     }
 }
 
 template <class T>
-void BinarySearchTree<T>::print(){
+void BinarySearchTree<T>::print_sorted(){
     if (root){
-        Node<T>* temp = root;
-        while (temp){
-            std::cout << temp->value << ' ';
-            temp = temp->left;
+        if(root->left){
+            print_sorted(root->left);
+        }
+        std::cout << root->value << ' ';
+        if (root->right){
+            print_sorted(root->right);
         }
     }
 
@@ -91,6 +88,14 @@ void BinarySearchTree<T>::print(){
 }
 
 template <class T>
-void BinarySearchTree<T>::print(Node<T>* node){
-
+void BinarySearchTree<T>::print_sorted(Node<T>* node){
+    if (node){
+        if(node->left){
+            print_sorted(node->left);
+        }
+        std::cout << node->value << ' ';
+        if (node->right){
+            print_sorted(node->right);
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 
 int main(){
     BinarySearchTree<int> my_tree;
-    my_tree.print();
-    my_tree.insert(4);
+    my_tree.insert(5);
     my_tree.insert(3);
     my_tree.insert(2);
-    my_tree.print();
+    my_tree.insert(6);
+    my_tree.insert(0);
+    my_tree.insert(-10);
+    my_tree.insert(-10);
+
+    my_tree.print_sorted();
     return 0;
 }
