6b8c72460ca5fc5eb002c7c6f3a2765fab909f53
DuParkLinux <dupark3@gmail.com>
2018-05-04 20:18:34 -0400

    Search() function implemented to return a pointer to the node with an element passed as argument. Returns null pointer if not found

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -14,11 +14,14 @@ public:
     BinarySearchTree() : root(0) { }
     BinarySearchTree(T val) : root(new Node<T>(val)) { }
 
+    Node<T>* search(const T&);
     void insert(const T&);
+    void remove(const T&);
     void print_sorted();
 
 private:
     Node<T>* root;
+    Node<T>* search(Node<T>*&, const T&);
     void insert(Node<T>*&, Node<T>*&, const T&);
     void print_sorted(Node<T>*);
 };
@@ -30,6 +33,9 @@ friend class BinarySearchTree<T>;
 public:
     Node() : left(0), right(0) { }
     Node(T val) : value(val), left(0), right(0) { }
+
+    T get_value() { return value; }
+
 private:
     T value;
     Node<T>* left;
@@ -38,6 +44,36 @@ private:
 
 
 /******************* Member and Nonmember Function Definitions ***************/
+
+template <class T>
+Node<T>* BinarySearchTree<T>::search(const T& val){
+    if(root){
+        if (val < root->value){
+            return search(root->left, val);
+        } else if (val > root->value){
+            return search(root->right, val);
+        } else if (val == root->value){
+            return root;
+        }
+    } else {
+        return 0;
+    }
+}
+
+template <class T>
+Node<T>* BinarySearchTree<T>::search(Node<T>*& node, const T& val){
+    if(node){
+        if (val < node->value){
+            return search(node->left, val);
+        } else if (val > node->value){
+            return search(node->right, val);
+        } else if (val == node->value){
+            return node;
+        }
+    } else {
+        return 0;
+    }
+}
 
 template <class T>
 void BinarySearchTree<T>::insert(const T& val){
@@ -55,13 +91,13 @@ void BinarySearchTree<T>::insert(const T& val){
 
 template <class T>
 void BinarySearchTree<T>::insert(Node<T>*& child, Node<T>*& parent, const T& val){
-    if (child){
+    if (child){ 
         if(val < child->value){
             insert(child->left, child, val);
         } else if (val > child->value){
             insert(child->right, child, val);
         }
-    } else { // node does not exist, child is where we insert new child
+    } else { // node does not exist, child is where we insert the element
         if (val < parent->value){
             child = new Node<T>(val);
             parent->left = child;
@@ -71,6 +107,8 @@ void BinarySearchTree<T>::insert(Node<T>*& child, Node<T>*& parent, const T& val
         }
     }
 }
+
+
 
 template <class T>
 void BinarySearchTree<T>::print_sorted(){
--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include "BinarySearchTree.h"
 
+using namespace std;
+
 int main(){
     BinarySearchTree<int> my_tree;
+    my_tree.print_sorted(); // attempting to print an empty tree
+
     my_tree.insert(5);
     my_tree.insert(3);
     my_tree.insert(2);
     my_tree.insert(6);
     my_tree.insert(0);
     my_tree.insert(-10);
-    my_tree.insert(-10);
 
-    my_tree.print_sorted();
+    my_tree.print_sorted(); // -10 0 2 3 5 6
+
+    Node<int>* node = my_tree.search(-10);
+    cout << node->get_value() << endl;;
+
+    
+
     return 0;
 }
